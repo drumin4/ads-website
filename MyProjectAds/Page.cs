df480281d093f8dc00d7d15b20216b86a9535316
Alex <alexandrumincu237@gmail.com>
2022-12-08 20:44:01 +0200

    Deleted some unneeded commentaries, removed the Privacy page from the website as it wasn't serving any purpose, and solved the bug that resulted in no page found when pressing the "My Favorites Ads" button

--- MyProjectAds/Page.cs
+++ MyProjectAds/Page.cs
@@ -12,48 +12,13 @@ namespace MyProjectAds
 
         public OrderBy OrderBy { get; set; }
 
-
-        //public Page()
-        //{
-        //    Varianta 1:
-
-        //    Ads = new List<Ad>();
-        //    Favorites = new List<Ad>();
-        //    {
-        //        new Ad
-        //        {
-        //            Id = 1,
-        //            Title = "Vand casa",
-        //            Details = "Vand casa Floresti, complet utilata",
-        //            Location = "Cluj",
-        //            Price = 520000,
-        //            Published = DateTime.Now.AddDays(-2),
-        //            Contact = "07xxxxx123",
-        //            ImageUrl = "https://www.eproiectedecase.ro/wp-content/uploads/2017/02/Proiect-casa-cu-Mansarda-18011-perspectiva2-1500x954.jpg"
-        //        },
-        //            new Ad
-        //            {
-        //                Id = 2,
-        //                Title = "Inchiriez apartament",
-        //                Details = "Inchiriez apartament - Brasov, centrul istoric",
-        //                Location = "Brasov",
-        //                Price = 450,
-        //                Published = DateTime.Now.AddDays(-1),
-        //                Contact = "0787234891",
-        //                ImageUrl = "https://nuscocitta.ro/wp-content/uploads/2017/12/Apartament-2-camere.jpg"
-        //            }
-        //}
-
-
-            public void ShowAll()
+        public void ShowAll()
         {
-                foreach(Ad item in Ads)
-                {
-                    item.ShowSummary();
-                }
+            foreach(Ad item in Ads)
+            {
+                item.ShowSummary();
+            }
         }
-
-            //Varianta 2:
 
         public void LoadData(string filePath)
         {
@@ -65,8 +30,6 @@ namespace MyProjectAds
             this.Ads = ads;
         }
     }
-
-    
 
     public enum OrderBy
     {
